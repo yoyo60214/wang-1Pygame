9a43ed96c00ced3eca0716ea9db55a034389f041
yoyo60214 <80554836+yoyo60214@users.noreply.github.com>
2021-04-10 15:33:55 +0800

    Add files via upload

---
+++ class 4.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 10 14:07:26 2021
+
+@author: EDISON
+"""
+
+import pygame
+BLACK=(0,0,0)
+WHITE=(155,243,0)
+pygame.init()#初始化
+size=(700,500)
+screen=pygame.display.set_mode(size)
+pygame.display.set_caption("我的遊戲")
+
+backgroundPic=pygame.image.load("7.jpg")
+backgroundPic=pygame.transform.scale(backgroundPic,size)
+backgroundPic.convert()
+shipPic=pygame.image.load("4.png")
+shipPic=pygame.transform.scale(shipPic,(100,100))
+shipPic.convert
+shootSound=pygame.mixer.Sound("6.ogg")
+ship_x=300
+ship_y=400
+
+
+
+
+
+done=False
+clock=pygame.time.Clock()
+while not done:
+    screen.fill(WHITE)
+    screen.blit(backgroundPic,(0,0))
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            done=True
+        if event.type==pygame.KEYDOWN:    
+            if event.key==pygame.K_SPACE:
+               shootSound.play()
+            if event.key==pygame.K_LEFT:
+                ship_x=ship_x-5
+            if event.key==pygame.K_RIGHT:
+                ship_x=ship_x+5
+   #############################
+   #do something
+    screen.blit(shipPic,(ship_x,ship_y)) 
+   
+    
+   
+            
+            
+    clock.tick(60)
+    pygame.display.flip()#更新畫面
+pygame.quit()
